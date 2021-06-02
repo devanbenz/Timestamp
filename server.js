b7d6c5862cdee25974c0165df3e4fdf22f3ee754
devanbenz <devandbenz@gmail.com>
2021-06-02 08:53:27 -0500

    Added gh actions for heroku deployment

---
+++ server.js
@@ -0,0 +1,52 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/', (req ,res) => {
+  const time = new Date()
+  res.json({
+    'unix': Date.parse(time),
+    'utc': time.toUTCString()
+  })
+})
+
+app.get('/api/:date', (req, res) => {
+  const { date } = req.params
+  const time = Date.parse(date)
+  if(!time){
+    res.json({
+      'unix':date*1,
+      'utc': new Date(Number(date)).toUTCString()
+    })
+  }else{
+    res.json({
+      'unix':time, 
+      'utc': new Date(date).toUTCString()
+    })
+  }
+})
+
+
+app.listen(8080, ()=> console.log('listening on 8080'))
